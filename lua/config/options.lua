f5ce97bbbe2246a6f06bc67a3f15f435f8323d2b
Emman-pip <parrenoemmanuel756@gmail.com>
2024-08-18 15:24:28 +0800

    Organize config files.

---
+++ lua/config/options.lua
@@ -0,0 +1,25 @@
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 999
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.termguicolors = true
+vim.opt.hlsearch = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cursorline = true
+vim.opt.virtualedit = "block"
+vim.opt.backupcopy = "yes"
+
+
+vim.loader.enable()
+-- for windows
+-- vim.opt.shell = "powershell.exe"
+
+-- for oil
+-- vim.keymap.set("n", "<Leader>m", ":Explore<cr>")
+-- vim.keymap.set("n", "<Leader>m", ":Oil<CR>")
