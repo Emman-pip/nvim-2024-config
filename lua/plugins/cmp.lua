7e9ff885b970626d91b0a005b9ecd178daf5597f
Emman-pip <parrenoemmanuel756@gmail.com>
2024-08-18 16:44:32 +0800

    Add borders to completion windows.

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -16,22 +16,22 @@ return {
 	},
 
 	config = function()
-		local cmp = require'cmp'
+		local cmp = require 'cmp'
 
 		cmp.setup({
 			snippet = {
 				-- REQUIRED - you must specify a snippet engine
 				expand = function(args)
 					-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-				require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+					require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
 					-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
 					-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 					-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
 				end,
 			},
 			window = {
-				-- completion = cmp.config.window.bordered(),
-				-- documentation = cmp.config.window.bordered(),
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
 			},
 			mapping = cmp.mapping.preset.insert({
 				['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -60,7 +60,7 @@ return {
 				{ name = 'buffer' },
 			})
 		})
-		require("cmp_git").setup() ]]-- 
+		require("cmp_git").setup() ]] --
 
 		-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 		cmp.setup.cmdline({ '/', '?' }, {
