89f0d00f1ea48a690d32b7c9ffa2639f45a57dfd
Emman-pip <parrenoemmanuel756@gmail.com>
2024-08-18 16:15:04 +0800

    Download linters by default through mason.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,6 +1,16 @@
 return {
-    "williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({ })
-		end,
+	"williamboman/mason.nvim",
+	config = function()
+		require("mason").setup({
+			ensure_installed = { 
+				"stylua",
+				"isort",
+				"black",
+				"prettierd",
+				"prettier",
+				"curlylint",
+				"clang-format",
+			}
+		})
+	end,
 }
