528904f384fb4215f8ae00e3c273a80fbdeea9b7
Emman-pip <parrenoemmanuel756@gmail.com>
2024-09-03 09:08:38 +0800

    Add code runner.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,6 +6,15 @@ vim.keymap.set("n", "<Leader>b", ":bprevious<CR>")
 vim.keymap.set("n", "<Leader>n", ":bnext<CR>")
 vim.keymap.set("n", "<Leader>d", ":bdelete<CR>")
 
+-- for code run
+vim.keymap.set('n', '<leader>r', ':RunCode<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>rf', ':RunFile<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>rft', ':RunFile tab<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>rp', ':RunProject<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>rc', ':RunClose<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>crf', ':CRFiletype<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>crp', ':CRProjects<CR>', { noremap = true, silent = false })
+
 -- telescope keymaps
 -- local builtin = require('telescope.builtin')
 -- vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
